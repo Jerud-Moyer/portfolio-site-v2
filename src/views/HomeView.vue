7c85ee852c967e61ae9dd3b0d611eaf198919a3c
Jerud Moyer <jerudmoyer@Jeruds-Most-Excellent-Machine.local>
2026-04-19 12:14:44 -0700

    add initial app

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Logo from '@/components/Logo.vue';
+
+
+</script>
+
+<template>
+  <main>
+    <Logo 
+      outlineColor="#FFFFFF"
+      fillColor="transparent"
+    />
+    <Logo
+      :gradient-fill="true"
+      
+    />
+    <Logo
+      :gradient-fill="true"
+      gradient-color-one="#3c3c3b"
+      gradient-color-two="#5B8E7D" 
+      outline-color="#B07156"   
+    />
+    <Logo
+      outline-color="#000"
+      fill-color="#B07156"
+      back-fill-color="#558B6E"
+    />
+  </main>
+</template>
