5719230d30c562ebd9ba4696484ed2a925bcb8b1
Jerud Moyer <jerudmoyer@Jeruds-Most-Excellent-Machine.local>
2026-04-20 08:28:43 -0700

    add tailwind and primevue with some basic theme configs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+// @ts-ignore
+import primeTheme from './theme'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +13,17 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: primeTheme,
+    options: {
+      darkModeSelector: 'system',
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue',
+      },
+    },
+  },
+})
 
 app.mount('#app')
